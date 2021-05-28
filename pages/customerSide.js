2b66a1b132ef13394789878b02d01957b3df522a
G. H. Krishna Chaitanya <g.h.krishnachaitanya@Gs-Mac-mini.local>
2021-05-28 10:38:13 +0530

    Started working on the store_management next.js project

---
+++ pages/customerSide.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from '../styles/customerSide.module.css'
+function customerSide(){
+    return (
+        <>
+            <h1 classNam={styles.mainHeading}>Customer side</h1>
+        </>
+    )
+}
+
+export default customerSide
---
+++ pages/sellerSide.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../styles/sellerSide.module.css'
+
+function sellerSide(){
+    return(
+        <>
+            <h1 className={styles.mianHeading}>Seller side</h1>
+        </>
+    )
+}
+
+export default sellerSide
