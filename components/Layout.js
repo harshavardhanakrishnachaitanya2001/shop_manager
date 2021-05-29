7df7fea6b58235e66d83176a0388b233c6de27c5
G. H. Krishna Chaitanya <g.h.krishnachaitanya@Gs-Mac-mini.local>
2021-05-29 15:18:19 +0530

    Change of plans

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,9 @@
 import React from 'react'
-import Nav from './Nav'
+
 const Layout = ({children})=> {
     return (
         <>
-            <Nav/>
-            <div>
                 {children}
-            </div>
         </>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,73 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const [name,setName]=useState('');
+  const [phoneNumber,setPhoneNumber]=useState();
+  const [customerAddress,setCustomerAddress]=useState('');
+  const [item,setItem]=useState('');
+  const [quantity,setQuantity]=useState();
+  const [customerDetails,setCustomerDetails]=useState([]);
+
+  const handleNameChange = (e) => {
+    setName(e.target.value)
+  }
+
+  const handlePhoneNumberChange = (e) => {
+    setPhoneNumber(e.target.value)
+  }
+
+  const handleAddressChange = (e) => {
+    setCustomerAddress(e.target.value)
+  }
+
+  const handleItemToBuy = (e) => {
+    setItem(e.target.value)
+  }
+
+  const handleQuantityChange = (e) => {
+    setQuantity(e.target.value)
+  }
+
+  const handleCustomerInfoSubmission = (e) => {
+    e.preventDefault();
+    setCustomerDetails([{
+      "name": name,
+      "phoneNumber": phoneNumber,
+      "address": customerAddress
+    }]);
+  }
+
+  const customerInfo = customerDetails.map((d)=>{return (
+    <ul key={d.name}>
+      <li>{d.name}</li>
+      <li>{d.phoneNumber}</li>
+      <li>{d.address}</li>
+    </ul>
+  )})
+
   return (
     <div>
       <Head>
         <title>Store Manager</title>
       </Head>
       <body className={styles.body}>
-        <h1 className={styles.title}>Store Manager</h1>  
+        <h1 className={styles.title}>AKC online store</h1>
+        <form className={styles.form} onSubmit={handleCustomerInfoSubmission}>
+          <label>
+            Name:
+            <input type='text' onChange={handleNameChange} value={name}/>
+          </label>
+          <label>
+            Phone No:
+            <input type='text' value={phoneNumber} onChange={handlePhoneNumberChange}/>
+          </label>
+          <label>
+            Address:
+            <textarea onChange={handleAddressChange} value={customerAddress}/>
+          </label>
+          <button type='submit'>OK</button>
+        </form>
+        {customerInfo}
       </body>
     </div>
   )
